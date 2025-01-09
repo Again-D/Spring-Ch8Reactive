272ea4000425c766819191933a76db74cc20ef31
Again-D <nemcn5306@gmail.com>
2025-01-09 11:37:16 +0900

    CH8 Reactive - RSocket

--- PlaneFinder/src/main/java/com/thehecklers/planefinder/PlaneController.java
+++ PlaneFinder/src/main/java/com/thehecklers/planefinder/PlaneController.java
@@ -1,11 +1,13 @@
 package com.thehecklers.planefinder;
 
+import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import reactor.core.publisher.Flux;
 
 import java.io.IOException;
+import java.time.Duration;
 
 @Controller
 public class PlaneController {
@@ -16,11 +18,28 @@ public class PlaneController {
         this.pService = pService;
     }
 
+    // 4. PlaneFinderService와 연계된 Controller의 값 조정..
     @ResponseBody
     @GetMapping("/aircraft")
     public Flux<Aircraft> getAircrafts() throws IOException {
         return pService.getAircraft();
     }
-
+    
+    // RSocket 적용 - 2) 통신을 위한 연결 메서드 생성
+    @MessageMapping("acstream")     // 경로 기반이 아니어서 "/"를 사용하지 않음.
+    public Flux<Aircraft> getCurrentAircrafts() throws IOException {
+        return pService.getAircraft().concatWith(
+                Flux.interval(Duration.ofSeconds(1))
+                        .flatMap(
+                                l -> {
+                                    try {
+                                        return pService.getAircraft();
+                                    } catch (IOException e) {
+                                        throw new RuntimeException(e);
+                                    }
+                                })
+        );
+    }
+    
 
 }
--- aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/PositionController.java
+++ aircraft-positions/src/main/java/com/thehecklers/aircraftpositions/PositionController.java
@@ -2,19 +2,31 @@ package com.thehecklers.aircraftpositions;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.messaging.rsocket.RSocketRequester;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
 
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 @Controller
 public class PositionController {
     @NonNull
     private final AircraftRepository repository;
+    // RSocket 통신을 위한 객체 주입
+    private final RSocketRequester requester;
+
     private WebClient client =
             WebClient.create("http://localhost:7634/aircraft");
+
+    PositionController(AircraftRepository repository,
+                       RSocketRequester.Builder builder) {
+        this.repository = repository;
+        this.requester = builder.tcp("localhost",7635);
+    }
 
     @GetMapping("/aircraft")
     public String getCurrentAircraftPositions(Model model) {
@@ -35,5 +47,14 @@ public class PositionController {
 
         model.addAttribute("currentPositions", aircrafts);
         return "positions";
+    }
+
+    // RSocket 클라이언트 엔드포인트
+    @ResponseBody
+    @GetMapping(value = "/acstream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Aircraft> getCurrentACPositionStream() {
+        return requester.route("acstream")
+                .data("Requesting aircraft positions")
+                .retrieveFlux(Aircraft.class);
     }
 }
